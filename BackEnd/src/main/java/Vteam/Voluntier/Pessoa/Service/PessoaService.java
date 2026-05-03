f7e11073ad02982eda78bb5daa6760b91d8d758b
PedroMilani04 <milanipedrovagula@gmail.com>
2026-05-03 16:30:31 -0300

    registration

--- BackEnd/src/main/java/Vteam/Voluntier/Pessoa/Service/PessoaService.java
+++ BackEnd/src/main/java/Vteam/Voluntier/Pessoa/Service/PessoaService.java
@@ -1,5 +1,6 @@
 package Vteam.Voluntier.Pessoa.Service;
 
+import Vteam.Voluntier.Pessoa.DTOS.CadastroDTO;
 import Vteam.Voluntier.Pessoa.DTOS.LoginDTO;
 import Vteam.Voluntier.Pessoa.Model.PessoaModel;
 import Vteam.Voluntier.Pessoa.Repository.PessoaRepository;
@@ -9,7 +10,32 @@ import java.util.Optional;
 
 @Service
 public class PessoaService {
-    private PessoaRepository pessoaRepository;
+    private final PessoaRepository pessoaRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public boolean cadastroCliente(CadastroDTO dto) {
+        if (pessoaRepository.existsByEmail(dto.getEmail()) || pessoaRepository.existsByCPF(dto.getCPF())) {
+            return false;
+        }
+
+        PessoaModel novaPessoa = new PessoaModel(
+            null,
+            dto.getNome(),
+            dto.getDataNascimento(),
+            dto.getTelefone(),
+            dto.getCPF(),
+            dto.getEmail(),
+            dto.getSenha(),
+            null,
+            null
+        );
+
+        pessoaRepository.save(novaPessoa);
+        return true;
+    }
 
     public boolean validarLogin(LoginDTO loginDTO){ //Está mockado por enquanto
         String email = loginDTO.getEmail();
