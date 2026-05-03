f64fa3e03e481f98f2a9fcc0b434e5af46531cb7
unknown <nicolas.s.fernandes@unesp.br>
2026-05-03 02:20:12 -0300

    chore: add new Libraries

--- BackEnd/src/main/java/Vteam/Voluntier/VoluntierApplication.java
+++ BackEnd/src/main/java/Vteam/Voluntier/VoluntierApplication.java
@@ -1,13 +1,16 @@
 package Vteam.Voluntier;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableMongock
 @SpringBootApplication
 public class VoluntierApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VoluntierApplication.class, args);
+		System.out.println("TESTE ENV: " + System.getenv("MONGO_DB"));
 	}
 
 }
